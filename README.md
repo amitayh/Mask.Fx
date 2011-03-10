19f0241a19f6937206d3e332ad6fac2a517ac6c2
Amitay Horwitz <amitayh@gmail.com>
2011-03-10 20:12:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Mask.Fx
+===========
+
+Extends MooTools official Mask plugin to add fading functionality
+http://github.com/mootools/mootools-more/blob/master/Source/Interface/Mask.js
---
+++ Source/Mask.Fx.js
@@ -0,0 +1,54 @@
+/*
+---
+description: Extends MooTools official Mask plugin to add fading functionality
+
+license: MIT-style
+
+authors:
+- Amitay Horwitz
+
+requires:
+- More/Interface/Mask
+
+provides: [Mask.Fx]
+
+...
+*/
+
+Mask.Fx = new Class({
+
+    Extends: Mask,
+
+    options: {
+        start: 0,
+        end: 1,
+        fx: {
+            property: 'opacity',
+            link: 'cancel'
+        }
+    },
+
+    initialize: function(target, options){
+        this.parent(target, options);
+        this.element.set('tween', this.options.fx);
+        this.element.get('tween').set(this.options.fx.property, this.options.start);
+        this.element.setStyle('display', 'block');
+    },
+
+    showMask: function(){
+        this.hidden = false;
+        this.fireEvent('show');
+        this.element.get('tween').start(this.options.end).chain(function() {
+            this.fireEvent('showEnd');
+        }.bind(this));
+    },
+
+    hideMask: function(){
+        this.hidden = true;
+        this.fireEvent('hide');
+        this.element.get('tween').start(this.options.start).chain(function() {
+            this.fireEvent('hideEnd');
+        }.bind(this));
+    }
+
+});
